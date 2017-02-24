23dee690c110dbe773860a1778cb1729de00f00d
Carrie Brown <carrie.brown@huskers.unl.edu>
2017-02-24 17:20:51 -0600

    Modifications for UNK Tutorial

--- R/advanced/traditional.R
+++ R/advanced/traditional.R
@@ -1,7 +1,11 @@
 # LARGE data example - using airline.csv
 
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(ggplot2)) install.packages("ggplot2")
+if(!require(maps)) install.packages("maps")
+
 # Load airline.csv data
-flights <- read.table("airline_trunc.csv", sep=",", header=TRUE, stringsAsFactors=FALSE)
+flights <- read.table("./data/airline.csv", sep=",", header=TRUE, stringsAsFactors=FALSE)
 
 # Function to estimate birthmonth of aircraft by finding the month and year of first flight
 birthmonth <- function(y){
@@ -25,21 +29,53 @@ for (i in aircrafts) {
 }
 
 # Calculate flight age using the birthmonth
-flights$Age <- flights[,'Year'] * as.integer(12) + 
+test <- flights[,'Year'] * as.integer(12) + 
                 flights[,'Month'] - as.integer(acStart[flights[,'TailNum']])
+
+flights <- mutate(flights, Age = (Year*as.integer(12))+ Month - as.integer(acStart[TailNum]))
 
 # Generate linear model for response: ArrDelay and predictors: Age and Year 
 lm <- lm(ArrDelay ~ Age + Year, data=flights)
 summary(lm)
 
+# Convert Months from number to factor
+flights$Month <- factor(flights$Month)
+levels(flights$Month) <- month.abb
 
-# Generate boxplots for each month of arrivals - In process may need to use tdyr and dplyr
-boxplot(flights$ArrTime[flights$Month==1], names=month.name[1], at=1, width=rep(1,12))
-for(i in 2:12) {
-  boxplot(flights$ArrTime[flights$Month==i], names=month.name[i], add=TRUE, at=i)
-}
+# Select a subset of fields needed to graph arrival delays by month
+subset_month <- select(flights, Month, ArrDelay)
+# Remove any records missing Arrival delay data
+subset_month <- subset[!is.na(subset$ArrDelay),]
 
-summary <- vector(0)
-for(i in unique(flights$Month)) {
-  summary <- c(summary, summary(flights$ArrTime[flights$Month==i]))
-}
+# Create violin graph showing arrival delays by month
+ggplot(subset_month, aes(Month,ArrDelay, fill=factor(Month))) + 
+  geom_violin(aes(group=Month)) + 
+  labs(y = "Arrival Delay (in minutes)") + 
+  theme(legend.position="none") +
+  labs(title = "Average Flight Arrival Delay by Month")
+
+ggsave("ave_delay_by_month.jpg", width=9, height=6)
+  
+# Load state list for airport codes and join departure state to flights dataframe
+airport_codes <- read.csv("airport_codes.csv", col.names=c("OriginState", "Origin"), stringsAsFactors = FALSE)
+flights <- left_join(flights, airport_codes, by="Origin")
+
+# Create subset of data containing origin state and arrival delay
+subset_state <- select(flights, OriginState, ArrDelay)
+subset_state <- subset_state[!is.na(subset_state$ArrDelay),]
+subset_state <- group_by(subset_state, OriginState)
+subset_summary <- summarise(subset_state, AveDelay=mean(ArrDelay))
+
+# Create graphic of US States colored by average delay time
+map = map_data("state")
+ggplot(subset_summary, aes(fill=AveDelay)) +
+  geom_map(aes(map_id=OriginState), map=map) +
+  scale_fill_distiller(name = "Average Delay (mins)", palette = "Spectral", direction=-1) +
+  expand_limits(x=map$long, y=map$lat) +
+  theme_void() +
+  labs(title = "Average Flight Arrival Delay by State")
+
+ggsave("ave_delay_by_state.jpg", width=9, height=5)
+
+
+
--- matlab/parallel_main.m
+++ matlab/parallel_main.m
@@ -1,12 +1,21 @@
-%This MATALB program ("parallel_main.m") calls for a number of MATLAB workers
-%according to that specified in the slurm submit scritp ('--ntasks_per_node').
-%Each MATLAB worker is then asked to print to the screen (MATLAB command)
-%their assigned for-loop index "i". 
+% This MATALB program ("parallel_main.m") calls for a number of MATLAB workers
+% according to that specified in the slurm submit scritp ('--ntasks_per_node').
+% Each MATLAB worker is then asked to print to the screen (MATLAB command)
+% their assigned for-loop index "i". 
 
+% Retrieve the number of tasks_per_node from the scheduler
 number = str2num(getenv('SLURM_TASKS_PER_NODE'));
+
+% create cluster object and point JobStorageLocation to the tmp folder
+pc = parcluster('local');
+pc.JobStorageLocation = strcat('/tmp/',getenv('SLURM_JOB_ID'));
+
+% 
 poolObj = parpool(number);
+
 parfor i=1:poolObj.NumWorkers   
    i
 end
+
 delete(gcp('nocreate'));
 
