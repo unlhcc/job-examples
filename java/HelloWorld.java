3dff3ffbb7cb8145fba46ea3bdc9e055188417ab
Caughlin Bohn <cbohn4@unl.edu>
2025-02-13 15:34:18 -0600

    Basic Java Example

---
+++ java/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.lang.Thread;
+public class HelloWorld {
+	public static void main(String [] args){
+		for (int i=0; i<5; i++){
+			System.out.println("Hello World! I am at step " + i + " of the loop!");
+		}
+	}
+}
+
